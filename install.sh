8f9ba24c405693aa1a611f02a631bb1c8dc592f9
Willy Micieli <otechdo@otechdo.com>
2024-09-28 15:08:00 +0000

    add missing deps

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 REPOSITORY="https://github.com/otechdo/oh.git"
-pacman -Sy rustup --noconfirm
+pacman -Sy rustup git --noconfirm
 rustup default stable
 git clone "${REPOSITORY}" && cd oh || exit 1
 make && make install
