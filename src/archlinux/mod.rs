d4099ba4a122be984a20c2b7bdcdc61b460f2b0b
Willy Micieli <otechdo@otechdo.com>
2024-09-29 06:49:39 +0200

    fix bug

--- src/archlinux/mod.rs
+++ src/archlinux/mod.rs
@@ -1,24 +1,32 @@
+use crate::ai::gemini::assistant;
 use crate::archlinux::keyboard::configure_keyboard;
 use crate::archlinux::locales::configure_locale;
 use crate::archlinux::network::reflector::reflector;
 use crate::archlinux::packages::install::packages;
+use crate::archlinux::services::system::enable_services;
 use crate::archlinux::time::configure_timezone;
 use crate::os::Os;
 use mirroring::configure_archlinux_mirrors;
 use std::io::Error;
-use crate::archlinux::services::system::enable_services;
 
 pub mod keyboard;
 pub mod locales;
-pub mod services;
 pub mod mirroring;
 pub mod network;
 pub mod packages;
+pub mod services;
 pub mod time;
 
 pub async fn arch_install() -> Result<(), Error> {
     let mut app = Os::default();
-    assert!(configure_archlinux_mirrors(&mut app).await.is_ok());
+    #[cfg(feature = "ai")]
+    assert!(assistant(
+        "Configure Mirrors.",
+        "Configure the Pacman mirrors to select the fastest ones based on your location.",
+        "This will make package installation and updates faster.",
+        "Faster download speeds for packages\nReduced waiting time for installations and updates\nImproved overall system performance",
+        "pacman mirrors").await.is_ok());
+    assert!(configure_archlinux_mirrors(&mut app).is_ok());
     assert!(reflector(&mut app).await.is_ok());
     assert!(configure_keyboard(&mut app).await.is_ok());
     assert!(configure_timezone(&mut app).await.is_ok());
--- src/archlinux/services/system.rs
+++ src/archlinux/services/system.rs
@@ -1,13 +1,12 @@
+use crate::utils::confirm;
+use inquire::MultiSelect;
 use std::fs::{read_to_string, File};
 use std::io::Error;
 use std::process::Command;
-use inquire::MultiSelect;
-use crate::utils::confirm;
 
-pub fn socket_running() -> Vec<String>
-{
+pub fn socket_running() -> Vec<String> {
     update_systemctl_cache();
-    let mut services:Vec<String> = Vec::new();
+    let mut services: Vec<String> = Vec::new();
     let x = read_to_string("/tmp/sockets-running").expect("failed to read /tmp/sockets_running");
     for line in x.lines() {
         services.push(line.to_string());
@@ -15,10 +14,9 @@ pub fn socket_running() -> Vec<String>
     services
 }
 
-pub fn services_running() -> Vec<String>
-{
+pub fn services_running() -> Vec<String> {
     update_systemctl_cache();
-    let mut services:Vec<String> = Vec::new();
+    let mut services: Vec<String> = Vec::new();
     let x = read_to_string("/tmp/services-running").expect("failed to read /tmp/services-running");
     for line in x.lines() {
         services.push(line.to_string());
@@ -26,22 +24,20 @@ pub fn services_running() -> Vec<String>
     services
 }
 
-
-pub fn services_disabled() -> Vec<String>
-{
+pub fn services_disabled() -> Vec<String> {
     update_systemctl_cache();
-    let mut services:Vec<String> = Vec::new();
-    let x = read_to_string("/tmp/services-disabled").expect("failed to read /tmp/services-disabled");
+    let mut services: Vec<String> = Vec::new();
+    let x =
+        read_to_string("/tmp/services-disabled").expect("failed to read /tmp/services-disabled");
     for line in x.lines() {
         services.push(line.to_string());
     }
     services
 }
 
-pub fn socket_disabled() -> Vec<String>
-{
+pub fn socket_disabled() -> Vec<String> {
     update_systemctl_cache();
-    let mut services:Vec<String> = Vec::new();
+    let mut services: Vec<String> = Vec::new();
     let x = read_to_string("/tmp/sockets-disabled").expect("failed to read /tmp/sockets-disabled");
     for line in x.lines() {
         services.push(line.to_string());
@@ -49,47 +45,95 @@ pub fn socket_disabled() -> Vec<String>
     services
 }
 
-pub fn all_disabled() -> Vec<String>
-{
+pub fn all_disabled() -> Vec<String> {
     update_systemctl_cache();
-    let mut services:Vec<String> = Vec::new();
+    let mut services: Vec<String> = Vec::new();
     services.append(&mut services_disabled().to_vec());
     services.append(&mut socket_disabled().to_vec());
     services
 }
 
-pub fn all_running() -> Vec<String>
-{
+pub fn all_running() -> Vec<String> {
     update_systemctl_cache();
-    let mut services:Vec<String> = Vec::new();
+    let mut services: Vec<String> = Vec::new();
     services.append(&mut services_running().to_vec());
     services.append(&mut socket_running().to_vec());
     services
 }
-pub fn is_running(service:String)->bool
-{
+pub fn is_running(service: String) -> bool {
     services_running().contains(&service)
 }
 
-pub fn is_disabled(service:String)->bool
-{
+pub fn is_disabled(service: String) -> bool {
     services_disabled().contains(&service)
 }
 
-pub fn update_systemctl_cache()
-{
-    assert!(Command::new("systemctl").arg("list-units").arg("--type=socket").arg("--state=running").stdout(File::create("/tmp/sockets-running").expect("failed to create /tmp/sockets-running")).spawn().expect("failed to spawn /tmp/sockets-running").wait().expect("failed to wait for /tmp/sockets-running").success());
-    assert!(Command::new("systemctl").arg("list-units").arg("--type=socket").arg("--state=disabled").stdout(File::create("/tmp/sockets-disabled").expect("failed to create /tmp/sockets-disabled")).spawn().expect("failed to spawn /tmp/sockets-disabled").wait().expect("failed to wait for /tmp/sockets-disabled").success());
-    assert!(Command::new("systemctl").arg("list-units").arg("--type=service").arg("--state=running").stdout(File::create("/tmp/services-running").expect("failed to create /tmp/services-running")).spawn().expect("failed to spawn /tmp/services-running").wait().expect("failed to wait for /tmp/services-running").success());
-    assert!(Command::new("systemctl").arg("list-units").arg("--type=service").arg("--state=disabled").stdout(File::create("/tmp/services-disabled").expect("failed to create /tmp/services-disabled")).spawn().expect("failed to spawn /tmp/services-disabled").wait().expect("failed to wait for /tmp/services-disabled").success());
+pub fn update_systemctl_cache() {
+    assert!(Command::new("systemctl")
+        .arg("list-units")
+        .arg("--type=socket")
+        .arg("--state=running")
+        .stdout(
+            File::create("/tmp/sockets-running").expect("failed to create /tmp/sockets-running")
+        )
+        .spawn()
+        .expect("failed to spawn /tmp/sockets-running")
+        .wait()
+        .expect("failed to wait for /tmp/sockets-running")
+        .success());
+    assert!(Command::new("systemctl")
+        .arg("list-units")
+        .arg("--type=socket")
+        .arg("--state=disabled")
+        .stdout(
+            File::create("/tmp/sockets-disabled").expect("failed to create /tmp/sockets-disabled")
+        )
+        .spawn()
+        .expect("failed to spawn /tmp/sockets-disabled")
+        .wait()
+        .expect("failed to wait for /tmp/sockets-disabled")
+        .success());
+    assert!(Command::new("systemctl")
+        .arg("list-units")
+        .arg("--type=service")
+        .arg("--state=running")
+        .stdout(
+            File::create("/tmp/services-running").expect("failed to create /tmp/services-running")
+        )
+        .spawn()
+        .expect("failed to spawn /tmp/services-running")
+        .wait()
+        .expect("failed to wait for /tmp/services-running")
+        .success());
+    assert!(Command::new("systemctl")
+        .arg("list-units")
+        .arg("--type=service")
+        .arg("--state=disabled")
+        .stdout(
+            File::create("/tmp/services-disabled")
+                .expect("failed to create /tmp/services-disabled")
+        )
+        .spawn()
+        .expect("failed to spawn /tmp/services-disabled")
+        .wait()
+        .expect("failed to wait for /tmp/services-disabled")
+        .success());
 }
 
-pub async fn enable_services()-> Result<(),Error>
-{
+pub async fn enable_services() -> Result<(), Error> {
     loop {
-        let services = MultiSelect::new("Select services to enable :",all_disabled()).prompt().expect("");
+        let services = MultiSelect::new("Select services to enable :", all_disabled())
+            .prompt()
+            .expect("");
         for service in services {
-            assert!(Command::new("systemctl").arg("enable").arg(service).spawn().expect("failed to enable service").wait().expect("failed to wait service").success());
+            assert!(Command::new("systemctl")
+                .arg("enable")
+                .arg(service)
+                .spawn()
+                .expect("failed to enable service")
+                .wait()
+                .expect("failed to wait service")
+                .success());
         }
         if confirm("Activate another services ?").await.eq(&false) {
             break;
